12fa69dfec262794154f687e7904739705eabfe1
SylvainGuilbaud <sylvain.guilbaud@intersystems.com>
2024-02-09 18:30:19 +0100

    fhir-sql-builder

--- install.sh
+++ install.sh
@@ -1,59 +1,61 @@
-#!/bin/bash
-# Usage install.sh [instanceName]
+    #!/bin/bash
+    # Usage install.sh [instanceName]
 
-DIR=$(dirname $0)
-if [ "$DIR" = "." ]; then
-DIR=$(pwd)
-fi
+    DIR=$(dirname $0)
+    if [ "$DIR" = "." ]; then
+    DIR=$(pwd)
+    fi
 
-instanceName=$1
-password=password
+    instanceName=$1
+    password=password
 
-ClassImportDir=$DIR/src
-NameSpace=""FHIRHL7V2DEMO""
+    ClassImportDir=$DIR/src
+    NameSpace=""FHIRHL7V2DEMO""
 
-irissession $instanceName -U USER <<EOF 
-sys
-sys
-zn "HSLIB"
-Set appKey = "/fhirhl7v2demo/fhir/r4"
-Set strategyClass = "HS.FHIRServer.Storage.Json.InteractionsStrategy"
-Set metadataConfigKey = "HL7v40"
+    irissession $instanceName -U USER <<EOF 
+    sys
+    sys
+    zn "HSLIB"
+    Set appKey = "/fhirhl7v2demo/fhir/r4"
+    Set strategyClass = "HS.FHIRServer.Storage.Json.InteractionsStrategy"
+    Set metadataConfigKey = "HL7v40"
 
-//Install a Foundation namespace and change to it
-Do ##class(HS.HC.Util.Installer).InstallFoundation("$NameSpace")
-zn "$NameSpace" 
+    //Install a Foundation namespace and change to it
+    # Do ##class(HS.HC.Util.Installer).InstallFoundation("$NameSpace")
+    Do ##class(HS.Util.Installer.Foundation).Install("$NameSpace")
 
-// Install elements that are required for a FHIR-enabled namespace
-Do ##class(HS.FHIRServer.Installer).InstallNamespace()
+    zn "$NameSpace" 
 
-// Install an instance of a FHIR Service into the current namespace
-Do ##class(HS.FHIRServer.Installer).InstallInstance(appKey, strategyClass, metadataConfigKey)
-do ##class(Ens.Director).StopProduction()
-do \$system.OBJ.ImportDir("$ClassImportDir","*.cls","cdk",.errors,1)
+    // Install elements that are required for a FHIR-enabled namespace
+    Do ##class(HS.FHIRServer.Installer).InstallNamespace()
 
-zw \$classmethod("Ens.Director", "SetAutoStart", "FHIRHL7V2DEMOPKG.FoundationProduction", 0)
+    // Install an instance of a FHIR Service into the current namespace
+    Do ##class(HS.FHIRServer.Installer).InstallInstance(appKey, strategyClass, metadataConfigKey)
+    do ##class(Ens.Director).StopProduction()
+    do \$system.OBJ.ImportDir("$ClassImportDir","*.cls","cdk",.errors,1)
 
-set cspConfig = ##class(HS.Util.RESTCSPConfig).URLIndexOpen(appKey)
-set cspConfig.ServiceConfigName = "HS.FHIRServer.Interop.Service"
-set cspConfig.AllowUnauthenticatedAccess = 1
-zw cspConfig.%Save()
+    zw \$classmethod("Ens.Director", "SetAutoStart", "FHIRHL7V2DEMOPKG.FoundationProduction", 0)
 
-set strategy = ##class(HS.FHIRServer.API.InteractionsStrategy).GetStrategyForEndpoint(appKey)
-set config = strategy.GetServiceConfigData()
-set config.DebugMode = 4
-do strategy.SaveServiceConfigData(config)
+    set cspConfig = ##class(HS.Util.RESTCSPConfig).URLIndexOpen(appKey)
+    set cspConfig.ServiceConfigName = "HS.FHIRServer.Interop.Service"
+    set cspConfig.AllowUnauthenticatedAccess = 1
+    zw cspConfig.%Save()
+
+    set strategy = ##class(HS.FHIRServer.API.InteractionsStrategy).GetStrategyForEndpoint(appKey)
+    set config = strategy.GetServiceConfigData()
+    set config.DebugMode = 4
+    do strategy.SaveServiceConfigData(config)
 
 
-zn "%SYS"
-set props2("NameSpace") = "$NameSpace"
-set props2("DispatchClass") = "FHIRDemo.REST.Dispatch"
-set props2("CookiePath") = "/csp/demo/rest/"
-set props2("Description") = "Demo REST API"
-set props2("MatchRoles") = ":%All"
-set props2("AutheEnabled") = 64
-set tSC = ##class(Security.Applications).Create("/csp/demo/rest", .props2)
-zw tSCs
+    zn "%SYS"
+    set props2("NameSpace") = "$NameSpace"
+    set props2("DispatchClass") = "FHIRDemo.REST.Dispatch"
+    set props2("CookiePath") = "/csp/demo/rest/"
+    set props2("Description") = "Demo REST API"
+    set props2("MatchRoles") = ":%All"
+    set props2("AutheEnabled") = 64
+    set tSC = ##class(Security.Applications).Create("/csp/demo/rest", .props2)
+    zw tSCs
 
-halt
-EOF
+    halt
+    EOF
